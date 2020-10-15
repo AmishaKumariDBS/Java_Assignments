01874dc11f60b12a1d595600813940d09e754d1d
AmishaKumariDBS <amishakumari@dbs.com>
2020-10-15 14:03:21 +0800

    Added MethodInterceptor log file

--- Day 15/AOPExample/com.training.org/MethodInterceptor.java
+++ Day 15/AOPExample/com.training.org/MethodInterceptor.java
@@ -1,15 +1,21 @@
 package com.training.org;
 
 import org.aopalliance.intercept.MethodInvocation;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
 
 public class MethodInterceptor implements org.aopalliance.intercept.MethodInterceptor{
+    boolean add = true;
+    FileHandler f = new FileHandler("logfile.log", add);
+    Logger l = Logger.getLogger("com.training.org");
+    l.addHandler(f);
 
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
         Object obj;
-        System.out.println("Additional Concern before actual logic");
+        l.info("Additional Concern before actual logic");
         obj=invocation.proceed();
-        System.out.println("Additional cocern after actual logic");
+        l.info("Additional cocern after actual logic");
         return null;
     }
 
