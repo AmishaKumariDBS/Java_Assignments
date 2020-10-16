7a47c4b792b1130dfc2c9a92b546dded85268962
AmishaKumariDBS <amishakumari@dbs.com>
2020-10-16 14:20:00 +0800

    Updated MyApp and added logfile.

---
+++ Day 16/MyApp/src/main/java/com/training/org/GlobalProperties.java
@@ -0,0 +1,20 @@
+package com.training.org;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+@Component
+@ConfigurationProperties
+@Validated
+public class GlobalProperties {
+    @Max(5)
+    @Min(0)
+    private int threadPool;
+    @NotEmpty
+    private String email;
+    //getters and setters
+}
+
--- Day 16/MyApp/src/main/java/com/training/org/OrgApplication.java
+++ Day 16/MyApp/src/main/java/com/training/org/OrgApplication.java
@@ -1,23 +1,95 @@
 package com.training.org;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Arrays;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+
+
+@ConfigurationProperties(prefix = "mail")
 public class OrgApplication {
+
+    Logger l = LoggerFactory.getLogger(OrgApplication.class);
+    private String hostname;
+    private int port;
+    private String from;
+    private List<String> mailCollection;
+    private Map<String,String>  additionalHeaders;
+    private DatabaseProperties dbProp;
+
+    public Map<String,String> getAdditionalHeaders(){return additionalHeaders;}
+    public void setAdditionalHeaders(Map<String, String> additionalHeaders) {
+        this.additionalHeaders = additionalHeaders;
+    }
+
+    public DatabaseProperties getDbProp() {
+        return dbProp;
+    }
+
+    public void setDbProp(DatabaseProperties dbProp) {
+        this.dbProp = dbProp;
+    }
+
+    public List<String> getMailCollection() {
+        return mailCollection;
+    }
+
+    public void setMailCollection(List<String> mailCollection) {
+        this.mailCollection = mailCollection;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
     @GetMapping("/")
-    public String getDets(){return "Spring Application";}
+    public String getDetails() {
+
+        l.debug("This is debug message");
+        l.info("INFO Message");
+        l.warn("WARN Message");
+        l.error("ERROR");
+        l.trace("This is a trace message");
+        return " "+ hostname+" "+port+" "+from+" "+mailCollection+dbProp+additionalHeaders;
+    }
+
     public static void main(String[] args) {
-
-    ApplicationContext ctx = SpringApplication.run(OrgApplication.class, args);
-    String[] beanNames = ctx.getBeanDefinitionNames();
+        ApplicationContext ctx=SpringApplication.run(OrgApplication.class, args);
+        String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
-        for(String bName: beanNames)
-            System.out.println(bName);
-}
-
+        for (String beanName : beanNames){
+            System.out.println(beanName);
+        }
+    }
 }
