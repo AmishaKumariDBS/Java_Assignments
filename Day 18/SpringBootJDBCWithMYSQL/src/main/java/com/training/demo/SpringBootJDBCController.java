ca975c9dbb3fdf941cee008458616a64da78e5b3
AmishaKumariDBS <amishakumari@dbs.com>
2020-10-21 01:25:46 +0800

    Spring Boot JDBC with MYSQL

---
+++ Day 18/SpringBootJDBCWithMYSQL/src/main/java/com/training/demo/SpringBootJDBCController.java
@@ -0,0 +1,17 @@
+package com.training.demo;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringBootJDBCController {
+    private JdbcTemplate jdbc;
+
+    @RequestMapping("/insert")
+    public String index() {
+        jdbc.execute("insert into user(name,email) values('EShan','eshan@gmail.com')");
+        return "Data inserted";
+    }
+
+}
